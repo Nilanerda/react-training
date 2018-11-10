2881480b7a560ef897e583a64342accbac93245d
Nilanerda <zzizzu@ukr.net>
2018-11-10 07:25:20 +0200

    created js file

---
+++ config.js
@@ -0,0 +1,12 @@
+var userNumber = +prompt("Ваше число");
+var radical = userNumber*userNumber;
+var addiction = userNumber + userNumber;
+var substraction = userNumber - userNumber;
+var multiplication = userNumber * userNumber;
+var division = userNumber / userNumber;
+
+alert("Ваше число в квадрате " + radical);
+alert("Мы сложили ваше число " + addiction);
+alert("Потом его вычли " + substraction);
+alert("Еще решили перемножить " + multiplication);
+alert("И на последок разделить " + division);
